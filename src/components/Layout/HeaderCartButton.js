948e70d2f6a3b823165d91dec160d3e00b3b7cde
Andjelija Milutinovic <milutinovici321@gmail.com>
2021-05-13 13:33:14 +0200

    'cart button added'

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,21 @@
+import CartIcon from '../Cart/CartIcon';
+//import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+    
+
+    return (
+        <button className={classes.buttonn} onClick={props.onClick}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>
+            3
+        </span>
+        </button>
+    );
+};
+
+export default HeaderCartButton;
